b108b26d6f66e3f9d99f500917c0e97044a39d45
se0kjun <hong921122@gmail.com>
2016-05-13 01:40:54 +0900

    react base settings

--- js/content_script.js
+++ js/content_script.js
@@ -1,9 +1,55 @@
-'use strict';
+var TabMenu = React.createClass({
+    displayName: "TabMenu",
 
-(function() {
-    var editor_box = $('.timeline-comment');
+    propTypes: {
+        itemList: React.PropTypes.array.isRequired
+    },
 
-    $.get(chrome.extension.getURL('html/emoji_box.html'), function(d) {
-        $('.timeline-comment .discussion-topic-header').append(d);
-    });
-})();
+    render: function render() {
+        var childLink = this.props.itemList.map(function(m) {
+            return React.createElement(TabLink, {itemName: m});
+        });
+        
+        return React.createElement('div', {}, childLink);
+    }
+});
+
+var TabItem = React.createClass({
+    propTypes: {
+        emojiList: React.PropTypes.array.isRequired,
+        emojiName: React.PropTypes.string.isRequired
+    },
+    
+    render: function render() {
+        return React.createElement('div', {})
+    }
+});
+
+var TabLink = React.createClass({
+    propTypes: {
+        itemName: React.PropTypes.string.isRequired
+    },
+
+    render: function render() {
+        return React.createElement('a', {className: 'item', data-tab: this.props.itemName}, this.props.itemName);
+    }
+});
+
+
+var HelloMessage = React.createClass({
+  displayName: "HelloMessage",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      null,
+      "Hello ",
+      this.props.name
+    );
+   }
+});
+
+ReactDOM.render(
+    React.createElement(HelloMessage, { name: "John" }), 
+    document.getElementById('emoji_selectmenu')
+);
