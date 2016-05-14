3f5431fbd4d85fc332c731615f2277fa62c38936
SeokJun Hong <hong921122@gmail.com>
2016-05-14 11:03:37 +0900

    add comopnent
    main component, tab, etc

--- js/content_script.js
+++ js/content_script.js
@@ -2,15 +2,19 @@ var TabMenu = React.createClass({
     displayName: "TabMenu",
 
     propTypes: {
-        itemList: React.PropTypes.array.isRequired
+        itemList: React.PropTypes.array.isRequired,
+        itemName: React.PropTypes.string
     },
 
-    render: function render() {
-        var childLink = this.props.itemList.map(function(m) {
-            return React.createElement(TabLink, {itemName: m});
+    render: function() {
+        var childElem = Object.keys(this.props.itemList).map(function(m) {
+            return React.createElement(TabLink, {itemName: this.props.itemList[m]});
         });
-        
-        return React.createElement('div', {}, childLink);
+      
+        return React.createElement('div', 
+        {
+            className: 'ui top attached tabular menu'
+        }, childLink);
     }
 });
 
@@ -20,8 +24,32 @@ var TabItem = React.createClass({
         emojiName: React.PropTypes.string.isRequired
     },
     
-    render: function render() {
-        return React.createElement('div', {})
+    render: function() {
+        if (Object.keys(this.props.emojiList) !== undefined) {
+            // exists key: not array
+            var childMenu = React.createElement(TabMenu, 
+                {
+                    itemList: this.props.emojiList, 
+                    itemName: this.props.emojiName
+                });
+          
+            return React.createElement('div', 
+            {
+                className: 'ui bottom attached tab segment'
+            }, childMenu);
+        }
+        else {
+            // array type: show emoji-items
+            var childEmoji = this.props.emojiList.map(function(m) {
+                return React.createElement(EmojiItem, {});
+            });
+
+            return React.createElement('div', 
+            {
+                className: 'ui bottom attached tab segment',
+                'data-tab': this.props.emojiName
+            }, childEmoji);
+        }
     }
 });
 
@@ -30,19 +58,61 @@ var TabLink = React.createClass({
         itemName: React.PropTypes.string.isRequired
     },
 
-    render: function render() {
-        return React.createElement('a', {className: 'item', data-tab: this.props.itemName}, this.props.itemName);
+    render: function() {
+        return React.createElement('a', 
+        {
+            className: 'item', 
+            'data-tab': this.props.itemName
+        }, this.props.itemName);
     }
 });
 
+var EmojiItem = React.createClass({
+    propTypes: {
+        emojiObject: React.PropTypes.object.isRequired
+    },
+
+    render: function() {
+    }
+});
+
+var MainRender = React.createClass({
+    getInitialState: function() {
+        return {
+            emojiObject: null
+        }
+    },
+
+    getEmojiObject: function() {
+        $.get('emoji.json', function(data) {
+            this.setState({
+                'emojiObject': data
+            });
+        }).bind(this);
+    },
+
+    componentWillDidMount: function() {
+        $('.tabular.menu .item').tab();
+    },
+
+    render: function() {
+        this.getEmojiObject();
+        var childMenu = React.createElement(TabMenu, { itemList: this.state.emojiObject });
+
+        return React.createElement('div',
+        {
+            className: 'emojibox'
+        }, childMenu);
+    }
+});
 
 var HelloMessage = React.createClass({
   displayName: "HelloMessage",
 
-  render: function render() {
+  render: render() {
     return React.createElement(
       "div",
-      null,
+      {className: 'test', 'data-tab': this.props.name},
       "Hello ",
       this.props.name
     );
