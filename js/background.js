880fa252e2afc1b069d42387fed36f59447d0541
se0kjun <hong921122@gmail.com>
2016-05-17 01:05:24 +0900

    execute all scripts when state updated

--- js/background.js
+++ js/background.js
@@ -3,7 +3,13 @@
 // Github uses HTML5's pushState for page transitions.
 // So the content script is forced to execute using webNavigation.
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    console.log('test');
+    chrome.tabs.executeScript(null,{file:"js/vendor/jquery-2.2.3.min.js"});
     chrome.tabs.executeScript(null,{file:"js/common.js"});
+    chrome.tabs.executeScript(null,{file:"js/vendor/semantic.min.js"});
+    chrome.tabs.executeScript(null,{file:"js/vendor/browser.min.js"});
+    chrome.tabs.executeScript(null,{file:"js/vendor/react.min.js"});
+    chrome.tabs.executeScript(null,{file:"js/vendor/react-dom.min.js"});
+    chrome.tabs.executeScript(null,{file:"js/vendor/react-with-addons.min.js"});
     chrome.tabs.executeScript(null,{file:"js/content_script.js"});
 });
+
