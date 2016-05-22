7180b1c5fb4d16cdfb08b70da5dd373e08208718
se0kjun <hong921122@gmail.com>
2016-05-23 02:17:44 +0900

    editor coverage

--- js/common.js
+++ js/common.js
@@ -6,3 +6,7 @@ var emoji_menu = $('<div/>', {
 
 if ($('#emoji_selectmenu').length == 0)
     $('.timeline-comment > .discussion-topic-header').after(emoji_menu);
+
+if ($('#emoji_selectmenu').length == 0)
+    $('.timeline-comment > .previewable-comment-form').before(emoji_menu);
+
