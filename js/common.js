27ba51fa1852e16fb7554546b2336f4863ab480e
SeokJun Hong <hong921122@gmail.com>
2016-06-27 11:00:41 +0900

    apply other editors

--- js/common.js
+++ js/common.js
@@ -1,12 +1,32 @@
 'use strict';
 
 var emoji_menu = $('<div/>', {
-    id: 'emoji_selectmenu',
+    class: 'emoji_selectmenu',
 });
 
-if ($('#emoji_selectmenu').length == 0)
+if ($('.timeline-comment > .emoji_selectmenu').length == 0)
     $('.timeline-comment > .discussion-topic-header').after(emoji_menu);
 
-if ($('#emoji_selectmenu').length == 0)
+if ($('.timeline-comment > .emoji_selectmenu').length == 0)
     $('.timeline-comment > .previewable-comment-form').before(emoji_menu);
 
+function mutation(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.type === "childList") {
+            $(mutation.addedNodes[0]).find('.previewable-comment-form').before(emoji_menu);
+        }
+    });
+}
+
+document.querySelectorAll('table.diff-table > tbody').forEach(function(elem) {
+    var observer = new MutationObserver(mutation);
+    observer.observe(elem, { attributes: true, childList: true, characterData: true });
+});
+
+// $('table.diff-table').each(function(idx, elem) {
+//     var observer = new MutationObserver(mutation);    
+//     console.log(elem);
+//     observer.observe(elem, { attributes: true, childList: true, characterData: true });
+// });
+// observer.observe(document.querySelector('table.diff-table'), 
+//     { attributes: true, childList: true, characterData: true });
