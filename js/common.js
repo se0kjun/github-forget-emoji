6a3ed7aa1788fd00de42b81dbcaac41f5d871694
SeokJun Hong <hong921122@gmail.com>
2016-06-28 11:04:39 +0900

    close modal
    add button to close modal window

--- js/common.js
+++ js/common.js
@@ -18,11 +18,13 @@
         })
     );
 
+    var emoji_modal_close = $('<svg aria-label="Close" class="gh-emoji-modal-header-close" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12"><path d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"></path></svg>')
+        .click(function() {
+            $('#gh-emoji-modal-wrapper').toggleClass('gh-emoji-modal-hide');
+        });
+
     var emoji_modal = $('<div/>', {
         class: 'gh-emoji-modal'
-    }), emoji_modal_close = $('<i/>', {
-        class: 'fa fa-times',
-        'aria-hidden': 'true'
     }), emoji_header = $('<div/>', {
         class: 'gh-emoji-header'
     }).append(
